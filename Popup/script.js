616965d23107cfe30f93ceb0d8a57b7d98fd4e61
Jv2350 <tp8375735@gmail.com>
2024-05-13 21:43:04 +0530

    Pop-up

---
+++ Popup/script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup")
+
+function openPopup() {
+    popup.classList.add("open-popup");
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup"); s
+}
