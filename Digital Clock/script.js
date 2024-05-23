e809f8474303644a69d326a498461a6904b7cee1
Jv2350 <tp8375735@gmail.com>
2024-05-23 22:13:31 +0530

    Digital Clock

---
+++ Digital Clock/script.js
@@ -0,0 +1,12 @@
+
+let hrs = document.querySelector("#hrs");
+let min = document.querySelector("#min");
+let sec = document.querySelector("#sec");
+
+
+setInterval(() => {
+    let currentTime = new Date();
+    hrs.innerHTML = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+    min.innerHTML = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+    sec.innerHTML = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
+}, 1000)
